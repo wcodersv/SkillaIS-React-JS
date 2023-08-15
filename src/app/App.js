8a9ee5c5cd1e47b0953b8a8c113ff94cdf85c828
wcodersv <vik99a@yandex.ru>
2023-08-15 18:35:47 +0700

    fonts

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,10 @@
+import { ButtonBalanceIcon } from '../ui/ButtonBalanceIcon/ButtonBalanceIcon';
 import './styles/global.css';
 
 function App() {
   return (
     <div>
+      <ButtonBalanceIcon />
     </div>
   );
 }

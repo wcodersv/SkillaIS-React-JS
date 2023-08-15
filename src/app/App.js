6b94c01e9a375274c78982773dbccde347200336
Kirill Maslov <maslov.prod@gmail.com>
2023-08-15 21:28:42 +0800

    start ui component

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,14 @@
 import { ButtonBalanceIcon } from '../ui/ButtonBalanceIcon/ButtonBalanceIcon';
+import { IncomingCall } from '../ui/TableComponents/IconCall/IncomingCall/table component/icon/IncomingCall';
+import { OutgoingCall } from '../ui/TableComponents/IconCall/OutgoingCall/OutgoingCall';
 import './styles/global.css';
 
 function App() {
   return (
     <div>
       <ButtonBalanceIcon />
+      <IncomingCall />
+      <OutgoingCall/>
     </div>
   );
 }
---
+++ src/ui/TableComponents/IconCall/OutgoingCall/OutgoingCall.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CallSvg from '../OutgoingCall/Vector.svg'
+
+export const OutgoingCall = () => {
+  return (
+    <img src={CallSvg} alt="outgoing" />
+  )
+}

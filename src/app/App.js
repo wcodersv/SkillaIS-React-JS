4788abf21c901297706f58a5fa8d44af967dd2bb
Milena <milena0202@icloud.com>
2023-08-16 12:23:42 +0300

    New components

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,8 @@
-import './styles/global.css';
+import "./styles/global.css";
 
 function App() {
   return (
     <div>
-
     </div>
   );
 }
--- src/ui/CallSearch/CallSearch.jsx
+++ src/ui/CallSearch/CallSearch.jsx
@@ -1,8 +1,17 @@
 import s from "./CallSearch.module.css"
+import { CloseButton } from "../CloseButton/CloseButton"
+import { Magnifier} from "../Magnifier/Magnifier"
+
 export const CallSearch = () => {
   return (
     <>
-    <input className={s.search} placeholder="Поиск по звонкам"></input>
+      <div className={s.magnifier}>
+        <Magnifier />
+      </div>
+      <div className={s.button}>
+        <CloseButton />
+      </div>
+      <input className={s.search} placeholder="Поиск по звонкам"></input>
     </>
-  )
+  );
 }

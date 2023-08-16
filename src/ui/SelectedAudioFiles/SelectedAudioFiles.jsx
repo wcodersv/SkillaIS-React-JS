2f42e1f40b3e6d968476bdc383d99c818659ca50
wcodersv <vik99a@yandex.ru>
2023-08-16 14:20:17 +0700

    ui download,SelectedAudioFiles

---
+++ src/ui/SelectedAudioFiles/SelectedAudioFiles.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from './SelectedAudioFiles.module.css'
+import CloseButton from '../CloseButton'
+import Download from '../Download'
+
+export const SelectedAudioFiles = () => {
+    return (
+        <div className={style.body}>
+            <label className={style.select}>Выбрано <span>2 аудиофайла</span></label>
+            <label className={style.selectall}>Выбрать все <span>310</span></label>
+            <label className={style.recognize}>Распознать</label>
+            <Download />
+            <CloseButton />
+        </div>
+    )
+}

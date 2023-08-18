0848af0930b25c7c3332d80520e022da9b279cba
NikolayShapovalov <sh@povalov.ru>
2023-08-18 16:32:21 +0300

    Сделал свою бд и ее уже отрендерил в звонки

--- src/component/CallRow/CallRow.jsx
+++ src/component/CallRow/CallRow.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import style from "./CallRow.module.css";
 import IncomingCall from "../../ui/TableComponents/IconCall/IncomingCall";
+import OutgoingCall from "../../ui/TableComponents/IconCall/OutgoingCall";
 import Avatar1 from "../../ui/TableComponents/Avatar1";
 import WebIcon from "../../ui/WebIcon";
 import StatusAssessment from "../../ui/StatusAssessment";
 import DotRaiting from "../../ui/DotRating";
 
-export const CallRow = ({ time, avatar, number: phoneNumber, duration }) => {
+export const CallRow = ({
+  source,
+  time,
+  avatar,
+  number: phoneNumber,
+  duration,
+  status,
+  direction,
+}) => {
   return (
     <div className={style.gridrow}>
-      <IncomingCall />
+      {direction === "incoming" ? <IncomingCall /> : <OutgoingCall />}
       <p>{time}</p>
       <div className={style.employees}>
         <Avatar1 img={avatar} />
@@ -17,10 +26,10 @@ export const CallRow = ({ time, avatar, number: phoneNumber, duration }) => {
         <WebIcon />
       </div>
       <p>{phoneNumber}</p>
-      <p className={style.source}>Rabota.ru</p>
+      <p className={style.source}>{source}</p>
       <div className={style.grade}>
-        <DotRaiting />
-        <StatusAssessment />
+        <DotRaiting rate={status} />
+        <StatusAssessment rate={status} text={status} />
       </div>
       <p className={style.duration}>{duration}</p>
     </div>
--- src/ui/StatusAssessment/StatusAssessment.jsx
+++ src/ui/StatusAssessment/StatusAssessment.jsx
@@ -1,10 +1,22 @@
 /* eslint-disable react/react-in-jsx-scope */
 import s from "./StatusAssessment.module.css";
 
-export const StatusAssessment = () => {
+export const StatusAssessment = ({ rate, text }) => {
+  let callRate = s.great;
+
+  if (rate === "good") {
+    callRate = s.great;
+  } else if (rate === "bad") {
+    callRate = s.bad;
+  } else if (rate === "soso") {
+    callRate = s.good;
+  } else {
+    callRate = s.rejected;
+  }
+
   return (
     <>
-      <p className={s.great}>Отлично</p>
+      <p className={callRate}>{text}</p>
     </>
   );
 };

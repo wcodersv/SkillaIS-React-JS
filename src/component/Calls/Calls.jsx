7cc4c889bb3b030518ee9d8fecb3471f96d0e5cb
NikolayShapovalov <sh@povalov.ru>
2023-08-21 19:48:16 +0300

    Сделал фильтрацию, пока что тестовая, она много чего не учитывает, добавил список сегодняшниъ

--- src/component/Calls/Calls.jsx
+++ src/component/Calls/Calls.jsx
@@ -1,26 +1,107 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { useSelector } from "react-redux";
 import CallRow from "../CallRow";
 
-export const Calls = () => {
+export const Calls = ({ value }) => {
   const callListData = useSelector((state) => state.calls.calls);
-  console.log(callListData);
+  const date = new Date();
+  const year = date.getFullYear();
+  const hour = date.getHours();
+  const min = date.getMinutes();
+  let month = date.getMonth() + 1;
+  if (month < 10) {
+    month = "0" + month;
+  }
+  const day = date.getDate();
+  const now = `${year}-${month}-${day} ${hour}:${min}`;
+
+  const resultData = [
+    ...callListData,
+    {
+      avatar:
+        "https://images.unsplash.com/photo-1596075780750-81249df16d19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHBvcnRyYWl0fGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60",
+      id: "93af7aae-4f4d-444a-aa77-65186a93243",
+      time: now,
+      name: "Evans",
+      lastname: "Spears",
+      source: "Banana Company",
+      duration: "2:56",
+      rate: "good",
+      phone: "+7(980)723-132-277",
+      calldirect: "incoming",
+      profession: "operator",
+    },
+    {
+      avatar:
+        "https://images.unsplash.com/photo-1596075780750-81249df16d19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHBvcnRyYWl0fGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60",
+      id: "93af7aae-4f4d-444a-aa77-65186a986597",
+      time: now,
+      name: "Pitter",
+      lastname: "Parker",
+      source: "Yota",
+      duration: "3:23",
+      rate: "good",
+      phone: "+7(980)788-132-297",
+      calldirect: "incoming",
+      profession: "operator",
+    },
+  ];
+
+  const resarr = resultData.filter((item) => item.time === now);
+
+  console.log(resarr);
+  console.log(resultData[0].time.slice(0, 10));
+
+  const total = resultData.filter((item) => item.time != now);
 
   return (
     <>
-      {callListData &&
-        callListData.map((item, index) => (
-          <CallRow
-            source={item.source}
-            key={index}
-            avatar={item.avatar}
-            number={item.phone}
-            time={item.time.slice(11, 16)}
-            duration={item.duration}
-            status={item.rate}
-            direction={item.calldirect}
-          />
-        ))}
+      {resultData &&
+        resultData
+          .filter(
+            (item) =>
+              item.name.toLowerCase().trim().includes(value.trim()) ||
+              item.source.toLowerCase().trim().includes(value.trim()) ||
+              item.phone.toLowerCase().trim().includes(value.trim()),
+          )
+          .filter((item) => item.time === now)
+          .map((item) => (
+            <CallRow
+              source={item.source}
+              key={item.id}
+              avatar={item.avatar}
+              number={item.phone}
+              time={item.time.slice(11, 16)}
+              duration={item.duration}
+              status={item.rate}
+              direction={item.calldirect}
+            />
+          ))}
+      <div style={{ margin: 10 }}>
+        yesterday <sup style={{ color: "#899CB1" }}>{total.length}</sup>
+      </div>
+      {resultData &&
+        resultData
+          .filter(
+            (item) =>
+              item.name.toLowerCase().trim().includes(value.trim()) ||
+              item.source.toLowerCase().trim().includes(value.trim()) ||
+              item.phone.toLowerCase().trim().includes(value.trim()),
+          )
+          .filter((item) => item.time != now)
+          .map((item) => (
+            <CallRow
+              source={item.source}
+              key={item.id}
+              avatar={item.avatar}
+              number={item.phone}
+              time={item.time.slice(11, 16)}
+              duration={item.duration}
+              status={item.rate}
+              direction={item.calldirect}
+            />
+          ))}
     </>
   );
 };
--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Main.module.css";
 import UserPanel from "../../component/UserPanel";
 import FilterPanel from "../../component/FilterPanel";
@@ -6,13 +6,20 @@ import CallGrid from "../../component/CallGrid";
 import Calls from "../../component/Calls";
 
 export const Main = () => {
+  const [value, setValue] = useState("");
+
+  const changer = (evt) => {
+    setValue(evt.target.value);
+    console.log(value);
+  };
+
   return (
     <main className={style.body}>
       <UserPanel />
       <div className={style.main}>
-        <FilterPanel />
+        <FilterPanel handler={changer} value={value} />
         <CallGrid>
-          <Calls />
+          <Calls value={value} />
         </CallGrid>
       </div>
     </main>
